6871ab3e3648eb38deef50cd7af9f14a70fb9793
Nuria <ngarciacobas@danielcastelao.org>
2023-02-02 14:22:31 +0100

    Hecho el fragmento de resta observar variables

--- app/src/main/java/com/example/ejemplof/MainActivity.kt
+++ app/src/main/java/com/example/ejemplof/MainActivity.kt
@@ -5,20 +5,37 @@ import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import com.example.ejemplof.ui.main.MainViewModel
 import com.example.ejemplof.ui.main.MultiFragment
 import com.example.ejemplof.ui.main.SumaFragment
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var viewModel: MainViewModel
+    val otraClase by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val btnSuma: Button = findViewById(R.id.button)
         val btnResta: Button = findViewById(R.id.button2)
         btnSuma.setOnClickListener { replaceFragment(SumaFragment()) }
         btnResta.setOnClickListener { replaceFragment(MultiFragment()) }
+
+        /*otraClase.liveResultado.observe(
+            this,
+            Observer(
+                fun(resultado: Int) {
+                    val tvResultado: TextView = findViewById(R.id.resultado)
+                    tvResultado.setText(resultado)
+
+                }
+            )
+        )*/
     }
 
     private fun replaceFragment(fragment: Fragment) {
--- app/src/main/java/com/example/ejemplof/ui/main/MultiFragment.kt
+++ app/src/main/java/com/example/ejemplof/ui/main/MultiFragment.kt
@@ -1,11 +1,16 @@
 package com.example.ejemplof.ui.main
 
+import android.annotation.SuppressLint
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.lifecycle.Observer
 import com.example.ejemplof.R
 
 class MultiFragment : Fragment() {
@@ -18,9 +23,37 @@ class MultiFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         // TODO: Use the ViewModel
+        viewModel.livenum1.observe(
+            this,
+            Observer(
+                fun(num1: Int) {
+                    val tvNum1 = view?.findViewById<TextView>(R.id.resta1)
+
+                    tvNum1?.text = "Num1:  $num1"
+                }
+            )
+        )
+        viewModel.livenum2.observe(
+            this,
+            Observer(
+                @SuppressLint("SetTextI18n")
+                fun(num2: Int) {
+                    val tvNum2 = view?.findViewById<TextView>(R.id.resta2)
+
+                    tvNum2?.text = "Num2:  $num2"
+                }
+            )
+        )
+        /*val tvResultado = view?.findViewById<EditText>(R.id.numerosuma)
+        val btnSalir = view?.findViewById<Button>(R.id.button)
+        btnSalir?.setOnClickListener {
+            viewModel.result(Integer.valueOf(tvResultado?.text.toString()))
+        }*/
+
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
